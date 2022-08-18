17f6f5e3077a6737bd5b248be932269cbd70a22b
Git like me <aaronpqking.gmail.com>
2022-08-17 23:26:01 -0400

    ready for submission

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,39 @@
 var currenttime = moment().hour();
-console.log(currenttime);
+
+currentDay = moment().format("LLLL");
+
+document.getElementById("currentDayTitle").innerHTML = currentDay;
+
 var startTime = moment("8:00am", "h:mma").format("h:mma");
-console.log(startTime);
 var date = moment().format("L");
+
 function submit(time) {
   var input = document.getElementById(time).value;
   localStorage.setItem(time, input);
 }
-console.log(moment().hour());
+
 function write() {
-  for (i = 8; i < 18; i++) {
-    var display = localStorage.getItem(`${i}`) || " ";
-    console.log(i, display);
-    console.log(document.getElementById(`${i}`));
-    document.getElementById(`${i}`).value = display;
+  for (i = 8; i < 19; i++) {
+    var display = localStorage.getItem(`${i}`);
+    var element = document.getElementById(`${i}`);
+    element.value = display;
 
     if (i < currenttime) {
-      document.getElementById(`${i}`).disabled = true;
+      element.disabled = true;
+      element.style.backgroundColor = "#dc3545";
+      console.log(document.querySelectorAll(".timeout"))
+      document.querySelectorAll(".timeout")[i-8].style.backgroundColor =
+        "#dc3545";
+    } else if (i > currenttime) {
+      element.style.backgroundColor = "#28a745";
+      document.querySelectorAll(".timeout")[i - 8].style.backgroundColor = "#28a745";
+    } else {
+      element.style.backgroundColor = "#ffc107";
+      document.querySelectorAll(".timeout")[i-8].style.backgroundColor =
+        "#ffc107";
     }
   }
 }
-function test() {}
 
 // // var input = document.getElementById("event").value;
 // var time = document.getElementById("time").innerText;
